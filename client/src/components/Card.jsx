10bf481116b547e34126e2d5e2550aa84e7f7580
Sixtotk <sixtoorden@gmail.com>
2022-10-12 00:40:04 -0500

    avance

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,31 +1,20 @@
 import React from "react";
+import "./card.css"
 
 export default function Card({name, image, types }){
 
   const typesMapped = types?.map((e) => (
     <h5 key={e}>{e[0].toUpperCase() + e.slice(1)}</h5>
   ));
-  
+
   return(
-    <div>
-      
-      <h3>{name}</h3>
-      <img src= {image} alt="img not found"/>
-      <div>{typesMapped}</div>
-      
+    <div className="contenedor_card">
+      <div className="card">
+        <img src= {image} alt="img not found" className="img_card"/>
+        <h3 className="poke_name_card">{name}</h3>
+        <div className="types_card">{typesMapped}</div>  
+      </div>
      </div>
   )
 }
-
-// export default function Card({name, image, types, id}) {  
-//     return(
-//         <div>
-//              <NavLink className="none" to={`/pokemon/${id}`}>
-//                 <div>
-//                     <img className="img" src={image} alt="img not found"/* width="200px" height="250vh"*/ />
-//                     <h2>{name.charAt(0).toUpperCase() + name.slice(1)}</h2>                                 
-//                     <div>
-//                   <h3> Type: </h3>
-        
-//                 </div>
 
--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ImagenLanding from "./imagenes/basic-pokeball-cover-mcs4xg2ash4hj4e9.jpg"
+import Bienvenida from "./imagenes/f692fa9b3e13658a642fec16ea00ee7e.png"
+import "./LandingPage.css"
 
 export default function LandingPage(){
   return(
     <div>
-      <h1> Bienvenidos </h1>
       <Link to ='/home'>
-        <button>Ingresar</button>
+        <button className="botonLanding">Ingresar</button>
       </Link>
+      <div className="grid-container">
+        <img className="imgLanding" src={ImagenLanding} alt="landing"/>
+        <img className="imgLanding" src={Bienvenida} alt="bienvenida"/>
+      </div>
+      
     </div>
   )
 }
