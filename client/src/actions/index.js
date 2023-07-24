3f7c84c54db29b7a07a6a3e83df5cd77f1494c11
Sixtotk <sixtoorden@gmail.com>
2023-07-24 15:47:15 -0500

    todo funciona con DB

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,7 +6,7 @@ export const GET_TYPES = 'GET_TYPES'
 export function getPokemons() {
   return async function (dispatch) {
     try {
-      var json = await axios.get("http://localhost:3001/pokemons");
+      var json = await axios.get("https://pokemon-app-ycbn.onrender.com/pokemons");
       return dispatch({
         type: GET_POKEMONS,
         payload: json.data,
@@ -31,7 +31,7 @@ export function getTypes(){
 export function getPokemonsbyName(name){
   return async function(dispatch){
   try{
-    var json = await axios.get(`http://localhost:3001/pokemons?name=${name}`);
+    var json = await axios.get(`https://pokemon-app-ycbn.onrender.com/pokemons?name=${name}`);
     return dispatch({
       type: 'GET_NAME_POKEMON',
       payload: json.data
@@ -43,7 +43,7 @@ export function getPokemonsbyName(name){
 }
 export function getDetail(id){
   return async function(dispatch){
-    var json = await axios.get(`http://localhost:3001/pokemons/${id}`);
+    var json = await axios.get(`https://pokemon-app-ycbn.onrender.com/pokemons/${id}`);
     return dispatch({
       type: 'GET_DETAIL',
       payload: json.data
@@ -54,7 +54,7 @@ export function getDetail(id){
 
 export function postPokemon(payload){
   return async function(dispatch){
-    var json = await axios.post("http://localhost:3001/pokemons",payload);
+    var json = await axios.post("https://pokemon-app-ycbn.onrender.com/pokemons",payload);
     
     return json
   }
